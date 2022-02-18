a2334c89d0a008e83af52ea449bc253c261219a5
brGuirra <br.guirra@gmail.com>
2022-02-18 20:46:16 -0300

    feat: set up UICollectionView

---
+++ project-10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  project-10
+//
+//  Created by Bruno Guirra on 18/02/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
